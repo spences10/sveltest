6cadf33f59ccbe07ad200f85a8764f9ec2ef4f14
spences10 <spences10apps@gmail.com>
2024-12-21 10:09:26 +0000

    chore(deps): add jsdom@25.0.1 and update vitest configuration to use jsdom environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom',
+		globals: true,
 	},
 });
