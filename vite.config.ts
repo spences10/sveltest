cb6487b80852b4ae2adaa713c96321b6c80c34d9
spences10 <spences10apps@gmail.com>
2024-12-21 17:23:43 +0000

    fix: correct typo in Vite configuration plugins array

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), svelteTesting()2],
+	plugins: [sveltekit(), svelteTesting()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
